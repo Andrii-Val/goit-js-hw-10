a91d7c4687939167e106d41d5fcdb23724662aca
Andrii-Val <andrii.val1006@gmail.com>
2023-07-07 15:42:11 +0300

    sw

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { fetchBreeds } from './js/cat-api';
+fetchBreeds();
+
+import { toggleDescription } from './js/about';
+toggleDescription();
+
+import { loader } from './js/cat-api';
+
+window.onload = function () {
+  loader.style.display = 'none';
+};
